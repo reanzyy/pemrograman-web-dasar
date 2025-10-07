7788db96fb38b6f62dc6ef6048ccaf018b161e03
Adriansyah Suryawan <suryawanadriansyah4@gmail.com>
2025-10-07 19:35:04 +0700

    Add initial implementation of PHP calculations and outputs

---
+++ php-dasar/latihan-1/index.php
@@ -0,0 +1,57 @@
+<?php
+$nama = "John Doe";
+echo "Nama saya adalah: " . $nama;
+echo "<br><br>";
+
+$angka1 = 10;
+$angka2 = 5;
+$jumlah = $angka1 + $angka2;
+echo "Hasil penjumlahan: " . $jumlah;
+echo "<br><br>";
+
+$a = 20;
+$b = 7;
+$selisih = $a - $b;
+echo "Hasil pengurangan: " . $selisih;
+echo "<br><br>";
+
+$x = 3;
+$y = 4;
+$hasilKali = $x * $y;
+echo "Hasil perkalian: " . $hasilKali;
+echo "<br><br>";
+
+$nilai1 = 100;
+$nilai2 = 25;
+$hasilBagi = $nilai1 / $nilai2;
+echo "Hasil pembagian: " . $hasilBagi;
+echo "<br><br>";
+
+$bilangan1 = 17;
+$bilangan2 = 5;
+$sisa = $bilangan1 % $bilangan2;
+echo "Sisa pembagian: " . $sisa;
+echo "<br><br>";
+
+$umur = 25;
+$umurSebagaiString = (string)$umur;
+echo "Umur saya adalah: " . $umurSebagaiString . " tahun";
+echo "<br><br>";
+
+$panjang = 8;
+$lebar = 5;
+$luas = $panjang * $lebar;
+echo "Luas persegi panjang: " . $luas;
+echo "<br><br>";
+
+$sisi = 4;
+$volume = $sisi * $sisi * $sisi;
+echo "Volume kubus: " . $volume;
+echo "<br><br>";
+
+$nilai1 = 80;
+$nilai2 = 90;
+$nilai3 = 75;
+$rataRata = ($nilai1 + $nilai2 + $nilai3) / 3;
+echo "Rata-rata nilai: " . $rataRata;
+echo "<br><br>";
